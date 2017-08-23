41d69f352d35f12aae53d83602cb34c66415ab21
Tyler Truong <tylertruong@gmail.com>
2017-08-23 13:33:03 -0700

    stack PC up until sequentially add/remove items

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
 };
 
 
+Stack.prototype = {
+  size: function () {
+    if (this.stackSize < 0) {
+      this.stackSize = 0;
+    }
+    return this.stackSize;
+  },
+  push: function () {
+    this.stackSize++;
+  },
+  pop: function () {
+    this.stackSize--;
+  }
+
+};
