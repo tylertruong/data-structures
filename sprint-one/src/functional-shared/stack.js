dcf66599a50030e9fba31c1476f1beb8073f3d4b
Tyler Truong <tylertruong@gmail.com>
2017-08-23 12:14:19 -0700

    FS- reports a size of zero for a new stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.stackSize = 0;
+  extend (someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  pop: {},
+  push: {},
+  size: function () {
+    return this.stackSize;
+  }
+};
 
+var extend = function(obj) {
 
+  var preArgs = Array.prototype.slice.call(arguments);
+  var args = preArgs.slice(1);
+  _.each(args, function(item) {
+    _.each(item, function(value, keys) {
+      obj[keys] = value;
+    });
+  });
+};
