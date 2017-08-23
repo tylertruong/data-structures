071cd7441cc6e70eb1b6ef95e6020aea8a0515ab
Tyler Truong <tylertruong@gmail.com>
2017-08-23 13:10:09 -0700

    PT - stack up to sequentially additing and removing

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.stackSize = 0;
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function () {
+    this.stackSize++;
+  },
+  pop: function () {
+    this.stackSize--;
+  }, 
+  size: function() {
+    if (this.stackSize < 0) {
+      this.stackSize = 0;
+    }
+    return this.stackSize;
+  }
+};
 
 
