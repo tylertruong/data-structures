488217eb6b85e893d273e12f2b303e3296e4efad
Tyler Truong <tylertruong@gmail.com>
2017-08-23 13:27:40 -0700

    finished queue for PT

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = Object.create(queueMethods);
   someInstance.stackSize = 0;
+  someInstance.storage = {};
+  someInstance.firstItem = 0;
+  someInstance.lastItem = 0;
 
   return someInstance;
 };
 
 var queueMethods = {
-  enqueue: function () {
+  enqueue: function (value) {
     this.stackSize++;
+    var temp = this.firstItem;
+    this.storage[temp] = value;
+    this.firstItem++;
+    return this.storage[temp];
   },
   dequeue: function () {
     this.stackSize--;
+    var temp = this.lastItem;
+    this.lastItem++;
+    return this.storage[temp];
   },
   size: function () {
     if (this.stackSize < 0) {
