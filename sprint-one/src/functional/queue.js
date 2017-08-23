f65c0e5ab699ec11dc221be13f9074815fc396dd
Tyler Truong <tylertruong@gmail.com>
2017-08-23 11:51:53 -0700

    allow sequentially adding and removing items

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,24 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
+  var lastItem = 0;
+  var firstItem = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
     size++;
+    var temp = firstItem;
+    storage[temp] = value;
+    firstItem++;
+    return storage[temp];
   };
 
   someInstance.dequeue = function() {
     size--;
+    var temp = lastItem;
+    lastItem++;
+    return storage[temp];
   };
 
   someInstance.size = function() {
