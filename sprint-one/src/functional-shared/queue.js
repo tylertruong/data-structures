2a3b3e241fe994598a8b55cf2d976c55c8a99e4a
Tyler Truong <tylertruong@gmail.com>
2017-08-23 12:30:18 -0700

    FS- reports a size of zero for a new queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.stackSize = 0;
+
+
+  extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function () {
+    return this.stackSize;
+  } 
+};
 
 
+var extend = function(obj) {
+
+  var preArgs = Array.prototype.slice.call(arguments);
+  var args = preArgs.slice(1);
+  _.each(args, function(item) {
+    _.each(item, function(value, keys) {
+      obj[keys] = value;
+    });
+  });
+};
