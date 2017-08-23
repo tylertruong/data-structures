82807ba1941864fbf2c7249dd42da50f7b83f6b7
Tyler Truong <tylertruong@gmail.com>
2017-08-23 12:29:12 -0700

    allows sequentially additing and removing items

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,6 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
+  someInstance.storage = {};
   someInstance.stackSize = 0;
   extend (someInstance, stackMethods);
 
@@ -11,9 +12,12 @@ var Stack = function() {
 var stackMethods = {
   pop: function() {
     this.stackSize--;
+    return this.storage[this.stackSize];
   },
-  push: function () {
+  push: function (value) {
+    this.storage[this.stackSize] = value;
     this.stackSize++;
+    return this.storage[this.stackSize];
   },
   size: function () {
     if(this.stackSize < 0) {
