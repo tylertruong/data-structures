da8ee5dfd878bb9a3983bcf6eea450eb77b6adcb
Tyler Truong <tylertruong@gmail.com>
2017-08-23 20:05:30 -0700

    Linked List Functionality

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail) {
+      list.head = list.tail;
+      list.tail = Node(value);
+    } else {
+      list.tail = Node(value);
+      list.head = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      var temp = list.head.value;
+      list.head = list.tail;
+      return temp;
+    }
   };
 
   list.contains = function(target) {
+    for (var key in list) {
+      if (list[key]['value'] === target) {
+        return true;
+      }
+    } return false;
   };
 
   return list;
