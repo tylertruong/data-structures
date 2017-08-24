2406bad18fb85b67c9841a4d618d0d296dca3542
Tyler Truong <tylertruong@gmail.com>
2017-08-24 16:36:10 -0700

    finished assignment

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,67 @@
 var BinarySearchTree = function(value) {
+
+  var someInstance = Object.create(binarySearchMethods);
+
+  someInstance.value = value;
+
+  return someInstance;
 };
 
 
+binarySearchMethods = {};
+
+binarySearchMethods.insert = function (value) {
+
+  if(value < this.value) {
+    if(this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value); 
+    }
+  } else {
+    if(this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  }
+ 
+};
+
+binarySearchMethods.contains = function (value) {
+
+  if (value < this.value) {
+    if (this.left) {
+      return this.left.contains(value);
+    } else {
+      return false;
+    }
+  } else if (value > this.value) {
+    if (this.right) {
+      return this.right.contains(value);
+    } else {
+      return false;
+    }
+  } else {
+    return true;
+  }
+
+};
+ 
+
+binarySearchMethods.depthFirstLog = function (callback) {
+  callback(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(callback);
+  } 
+
+  if (this.right) {
+    this.right.depthFirstLog(callback);
+  }
+  
+};
+
+//var binaryTreeNode = 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
